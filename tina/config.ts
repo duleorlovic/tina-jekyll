5a5533ab97b8967eefaee548af4f4b3b95e9d38c
Dusan Orlovic <dule@trk.in.rs>
2025-09-08 09:13:27 +0200

    Add title so it matches the schema

--- tina/config.ts
+++ tina/config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "tinacms";
 
+import Page from "./jekyll/page";
+import Post from "./jekyll/post";
 // Your hosting provider likely exposes this as an environment variable
 const branch =
   process.env.GITHUB_BRANCH ||
@@ -21,33 +23,15 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "",
+      mediaRoot: "assets/uploads",
       publicFolder: "./",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
+      Page,
+      Post,
     ],
   },
 });
